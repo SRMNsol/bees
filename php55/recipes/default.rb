bdad8030409000c6d5712cc5959be1ef836ca906
Richard Hurt <rhurt@cafepress.com>
2014-05-24 22:13:09 -0400

    remove PHP/HTTPD; install PHP55

--- php55/recipes/default.rb
+++ php55/recipes/default.rb
@@ -1,33 +1,35 @@
-# Clean up "old" packages
-package 'php' do
-  action :remove
-end
-package 'php-xml' do
-  action :remove
-end
-package 'php-process' do
-  action :remove
-end
-package 'php-common' do
-  action :remove
-end
-package 'php-cli' do
-  action :remove
-end
-package 'httpd-tools' do
-  action :remove
-end
-package 'httpd' do
-  action :remove
-end
+case node[:platform]
 
-# Install PHP 5.5
-package 'php55' do
-  case node[:platform]
-  when 'centos','redhat','fedora','amazon'
-    package_name 'php55'
-  when 'debian','ubuntu'
-    package_name 'php'
+  when "amazon"
+    node.set['mysql']['server']['packages'] = %w{mysql55-server}
+    node.set['mysql']['client']['packages'] = %w{mysql55}
+
+    # remove any existing php/mysql
+    execute "yum remove -y php* httpd*"
+
+    # get the metadata
+    execute "yum -q makecache"
+
+    # manually install php 5.5
+    execute "yum install -y php55 php55-devel"
+    # execute "yum install -y php55 php55-devel php55-cli php55-snmp php55-soap php55-xml php55-xmlrpc php55-process php55-mysqlnd php55-pecl-memcache php55-opcache php55-pdo php55-imap php55-mbstring php55-intl"
+
+  when "rhel", "fedora", "suse", "centos"
+    node.set['php']['packages'] = ['php55', 'php55-devel', 'php55-cli', 'php55-snmp', 'php55-soap', 'php55-xml', 'php55-xmlrpc', 'php55-process', 'php55-mysqlnd', 'php55-pecl-memcache', 'php55-opcache', 'php55-pdo', 'php55-imap', 'php55-mbstring']
+    node.set['mysql']['server']['packages'] = %w{mysql55-server}
+    node.set['mysql']['client']['packages'] = %w{mysql55}
+
+    include_recipe "php"
+
+  when "debian"
+    include_recipe "apt"
+    apt_repository "wheezy-php55" do
+      uri "#{node['php55']['dotdeb']['uri']}"
+      distribution "#{node['php55']['dotdeb']['distribution']}-php55"
+      components ['all']
+      key "http://www.dotdeb.org/dotdeb.gpg"
+      action :add
+    end
+
+    include_recipe "php"
   end
-  action :install
-end
