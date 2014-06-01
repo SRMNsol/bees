a98ce28dfef756b6cabb3a0f01883829246c8792
Richard Hurt <rhurt@cafepress.com>
2014-05-31 23:28:38 -0400

    added mod_ssl recipe back

---
+++ apache2/recipes/mod_ssl.rb
@@ -0,0 +1,3 @@
+# The default OpsWorks install provides an older version of Apache & PHP and when we install
+# a newer version it conflicts with this module.  When OpsWorks gets updated then we can remove
+# this recipe and let it configure everything properly again.
