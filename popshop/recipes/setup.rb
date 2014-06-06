b980bb46aeabf55206e7f91b9eb4b596b4109229
Richard Hurt <rhurt@cafepress.com>
2014-06-05 22:36:08 -0400

    roll back to PHP 5.4

--- popshop/recipes/setup.rb
+++ popshop/recipes/setup.rb
@@ -1,10 +1,10 @@
-# Remove AuthZ support
-execute "a2dismod authz_default" do
-  user "root"
-end
+# # Remove AuthZ support
+# execute "a2dismod authz_default" do
+#   user "root"
+# end
 
 # Add a PPA for PHP 5.4
-execute "add-apt-repository ppa:ondrej/php5" do
+execute "add-apt-repository ppa:ondrej/php5-oldstable" do
   user "root"
 end
 
@@ -13,9 +13,10 @@ execute "aptitude update" do
   user "root"
 end
 
+
 # Upgrade PHP (as well as other things)
 # %w{apache2 apache2-mpm-prefork apache2.2-bin libapache2-mod-php5 php-pear php5 php5-cli php5-common php5-curl php5-dev php5-fpm php5-gd php5-mcrypt php5-memcache php5-mysql php5-sqlite php5-xmlrpc php5-xsl}.each do |pkg|
-%w{apache2 php5}.each do |pkg|
+%w{php5}.each do |pkg|
   package pkg do
     action :upgrade
   end
