77288724f9729bc543aa4c75160524d1e96d311f
Richard Hurt <rhurt@cafepress.com>
2014-06-05 21:38:33 -0400

    updating lots of packages manually

--- popshop/recipes/setup.rb
+++ popshop/recipes/setup.rb
@@ -4,11 +4,17 @@ execute "add-apt-repository ppa:ondrej/php5" do
 end
 
 # Update the APT system
-execute "apt-get update" do
+execute "aptitude update" do
   user "root"
 end
 
 # Upgrade PHP (as well as other things)
-execute "apt-get upgrade -y" do
-  user "root"
+%w{apache2 apache2-mpm-prefork apache2.2-bin libapache2-mod-php5 php-pear php5 php5-cli php5-common php5-curl php5-dev php5-fpm php5-gd php5-mcrypt php5-memcache php5-mysql php5-sqlite php5-xmlrpc php5-xsl}.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
 end
+
+# execute "aptitude upgrade -y" do
+#   user "root"
+# end
