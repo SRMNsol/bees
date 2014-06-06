49c942b0112785504189d057ccae630a9a305320
Richard Hurt <rhurt@cafepress.com>
2014-06-05 23:05:54 -0400

    Added libicu-dev package

--- popshop/recipes/setup.rb
+++ popshop/recipes/setup.rb
@@ -13,10 +13,9 @@ execute "aptitude update" do
   user "root"
 end
 
-
 # Upgrade PHP (as well as other things)
 # %w{apache2 apache2-mpm-prefork apache2.2-bin libapache2-mod-php5 php-pear php5 php5-cli php5-common php5-curl php5-dev php5-fpm php5-gd php5-mcrypt php5-memcache php5-mysql php5-sqlite php5-xmlrpc php5-xsl}.each do |pkg|
-%w{php5}.each do |pkg|
+%w{libicu-dev php5}.each do |pkg|
   package pkg do
     action :upgrade
   end
