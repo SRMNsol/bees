0ba3cabb6dd73dd5f415cb1e05357de7d88a25ac
Ben <benedict@phpdevel.net>
2014-07-11 13:51:30 +0200

    Modify php extensions

--- mod_php5_apache2/attributes/customize.rb
+++ mod_php5_apache2/attributes/customize.rb
@@ -6,7 +6,6 @@ normal[:mod_php5_apache2][:packages] = [
                                         'php5-xsl',
                                         'php5-curl',
                                         'php5-xmlrpc',
-                                        'php5-dev',
                                         'php5-gd',
                                         'php5-cli',
                                         'php5-mysql',
@@ -15,7 +14,6 @@ normal[:mod_php5_apache2][:packages] = [
                                         'php-pear',
                                         'php-xml-parser',
                                         'php-mail-mime',
-                                        'php-db',
-                                        'php-mdb2',
                                         'php-html-common',
-                                        'php-apc' ]
+                                        'php-apc',
+                                        'php5-intl' ]
