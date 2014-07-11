8f4278260d0c12eafcb2052014851b9e31b66af7
Ben <benedict@phpdevel.net>
2014-07-11 15:22:27 +0200

    Rewrite modules

--- mod_php5_apache2/attributes/customize.rb
+++ mod_php5_apache2/attributes/customize.rb
@@ -3,17 +3,16 @@
 ###
 
 normal[:mod_php5_apache2][:packages] = [
-                                        'php5-xsl',
-                                        'php5-curl',
-                                        'php5-xmlrpc',
-                                        'php5-gd',
-                                        'php5-cli',
-                                        'php5-mysql',
-                                        'php5-mcrypt',
-                                        'php5-memcache',
-                                        'php-pear',
-                                        'php-xml-parser',
-                                        'php-mail-mime',
-                                        'php-html-common',
-                                        'php-apc',
-                                        'php5-intl' ]
+  'php5-xsl',
+  'php5-curl',
+  'php5-xmlrpc',
+  'php5-sqlite',
+  'php5-dev',
+  'php5-gd',
+  'php5-cli',
+  'php5-mysql',
+  'php5-mcrypt',
+  'php5-memcache',
+  'php-apc',
+  'php5-intl'
+]
