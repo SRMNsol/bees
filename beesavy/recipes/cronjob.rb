85915f851b59e9e365b4d8c0d09f76d1e0ffba7b
Ben <benedict@phpdevel.net>
2014-07-25 18:21:32 +0200

    Simplify cronjobs

--- beesavy/recipes/cronjob.rb
+++ beesavy/recipes/cronjob.rb
@@ -4,59 +4,17 @@ node[:deploy].each do |app_name, deploy|
     next
   end
 
-  cron "popshops_networks" do
+  cron "beesavy_transactions" do
     user "deploy"
     hour "6"
     minute "0"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php api:networks --catalog-key=74762wn6w9utgw6khg2ta92gz"
+    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php beesavy:transactions"
   end
 
-  cron "linkshare_transactions" do
-    user "deploy"
-    hour "6"
-    minute "20"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php linkshare:transactions"
-  end
-
-  cron "cj_transactions" do
-    user "deploy"
-    hour "6"
-    minute "25"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php cj:transactions"
-  end
-
-  cron "pepperjam_transactions" do
-    user "deploy"
-    hour "6"
-    minute "30"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php pepperjam:transactions"
-  end
-
-  cron "pepperjam_transactions_update" do
-    user "deploy"
-    hour "6"
-    minute "35"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php pepperjam:transactions --update"
-  end
-
-  cron "shareasale_transactions" do
-    user "deploy"
-    hour "6"
-    minute "40"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php shareasale:transactions"
-  end
-
-  cron "shareasale_transactions_update" do
-    user "deploy"
-    hour "6"
-    minute "45"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php shareasale:transactions --update"
-  end
-
-  cron "referral_calculate" do
+  cron "beesavy_referral_calculate" do
     user "deploy"
     hour "7"
     minute "0"
-    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php beesavy:referral:calculate --exclude=pbbmedia@gmail.com"
+    command "cd #{deploy[:deploy_to]}/current && php ./bin/console.php beesavy:referral:calculate"
   end
 end
