ab773fe5455b506ae7e78df73107b3948810990a
Ben <benedict@phpdevel.net>
2014-08-14 17:49:03 +0200

    Fix chef debug log

--- beesavy/recipes/transactions.rb
+++ beesavy/recipes/transactions.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
   if deploy[:application_type] != 'php'
-    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    Chef::Log.debug("Skipping beesavy::transactions application #{app_name} as it is not an PHP app")
     next
   end
 
